b1e310ff55dd0d90bfa661135b814f8373e26c25
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-01-06 17:14:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 9090;
 
 //secrets
 require('dotenv').config();
