bbee79e61298bc9c56987a38e20a286bdc97c787
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2021-02-02 18:41:01 +0800

    Update Drill Down

--- public/js/query.js
+++ public/js/query.js
@@ -2,7 +2,7 @@ const EMPTY = { //NO input values
     'Roll-up': '',
     'Drill Down': "SELECT <br>"+
                   "   YEAR(o.order_approved_at) as 'Year', <br>" +
-                  "   c.customer_state as 'State', c.customer_city as 'City', <br>" +
+                  "   c.customer_state as 'State', o.payment_type as 'Payment_Type', <br>" +
                   "   count(o.order_id) as 'Number of orders' <br>" +
                   "FROM	<br>" +
                   "   order_customers c <br>" +
@@ -11,12 +11,12 @@ const EMPTY = { //NO input values
                   "GROUP BY <br>" +
                   "   YEAR(o.order_approved_at), <br>" +
                   "   c.customer_state, <br>" +
-                  "   c.customer_city WITH ROLLUP <br>" +
+                  "   o.payment_type WITH ROLLUP <br>" +
                   "UNION ( <br>" +  
                   "   SELECT <br>" +
                   "      null, <br>" +
                   "      c.customer_state as 'State', <br>" +
-                  "      c.customer_city as 'City', <br>" + 
+                  "      o.payment_type as 'Payment_Type', <br>" + 
                   "      count(o.order_id) as 'Number of orders' <br>" + 
                   "   FROM <br>" +
                   "      order_customers c <br>" +
@@ -24,14 +24,14 @@ const EMPTY = { //NO input values
 	              "   INNER JOIN orders o ON o.order_id = i.order_id <br>" +
                   "   GROUP BY <br>"+
                   "      c.customer_state, <br>" +
-                  "      c.customer_city <br>" +
+                  "      o.payment_type <br>" +
                   "      WITH ROLLUP<br>" +
                   ") <br>" + 
                   "UNION ( <br>" +  
                   "   SELECT <br>" +
                   "      YEAR(o.order_approved_at) as 'Year', <br>" +
                   "      null, <br>" +
-                  "      c.customer_city as 'City', <br>" + 
+                  "      o.payment_type as 'Payment_Type', <br>" + 
                   "      count(o.order_id) as 'Number of orders' <br>" + 
                   "   FROM <br>" +
                   "      order_customers c <br>" +
@@ -39,27 +39,27 @@ const EMPTY = { //NO input values
 	              "   INNER JOIN orders o ON o.order_id = i.order_id <br>" +
                   "   GROUP BY <br>"+
                   "      YEAR(o.order_approved_at), <br>" +
-                  "      c.customer_city <br>" +
+                  "      o.payment_type <br>" +
                   "      WITH ROLLUP<br>" +
                   ") <br>" +  
                   "UNION ( <br>" +  
                   "   SELECT <br>" +
                   "      null, <br>" +
                   "      null, <br>" +
-                  "      c.customer_city as 'City', <br>" + 
+                  "      o.payment_type as 'Payment_Type', <br>" + 
                   "      count(o.order_id) as 'Number of orders' <br>" + 
                   "   FROM <br>" +
                   "      order_customers c <br>" +
                   "   INNER JOIN order_items i ON c.customer_id = i.customer_id <br>" +
 	              "   INNER JOIN orders o ON o.order_id = i.order_id <br>" +
                   "   GROUP BY <br>"+
-                  "      c.customer_city <br>" +
+                  "      o.payment_type <br>" +
                   "      WITH ROLLUP<br>" +
                   ") <br>" + 
                   "ORDER BY <br>" +
                   "Year DESC, <br>" +
                   "State DESC, <br>" +
-                  "City DESC; <br>",
+                  "Payment_Type DESC; <br>",
         'Dice': "SELECT oc.customer_state as 'State', YEAR(o.order_approved_at) as 'Year ordered', MONTH(o.order_approved_at) as 'Month ordered', count(o.order_approved_at) as 'Number of orders' <br> " +
         "FROM order_items oi <br> " +
         "JOIN order_customers oc <br> " +
@@ -150,7 +150,7 @@ const INPUT = { //with input values
 
     'Drill Down Month': "SELECT <br>"+
     "   MONTH(o.order_approved_at) as 'Month', <br>" +
-    "   c.customer_state as 'State', c.customer_city as 'City', <br>" +
+    "   c.customer_state as 'State', o.payment_type as 'Payment_Type', <br>" +
     "   count(o.order_id) as 'Number of orders' <br>" +
     "FROM	<br>" +
     "   order_customers c <br>" +
@@ -160,12 +160,12 @@ const INPUT = { //with input values
     "GROUP BY <br>" +
     "   MONTH(o.order_approved_at), <br>" +
     "   c.customer_state, <br>" +
-    "   c.customer_city WITH ROLLUP <br>" +
+    "   o.payment_type WITH ROLLUP <br>" +
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      null, <br>" +
     "      c.customer_state as 'State', <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -174,14 +174,14 @@ const INPUT = { //with input values
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL <br> " +
     "   GROUP BY <br>"+
     "      c.customer_state, <br>" +
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      MONTH(o.order_approved_at) as 'Month', <br>" +
     "      null, <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -190,14 +190,14 @@ const INPUT = { //with input values
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL <br> " +
     "   GROUP BY <br>"+
     "      MONTH(o.order_approved_at), <br>" +
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      null, <br>" +
     "      null, <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -205,18 +205,18 @@ const INPUT = { //with input values
     "   INNER JOIN orders o ON o.order_id = i.order_id <br>" +
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL <br> " +
     "   GROUP BY <br>"+
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "ORDER BY <br>" +
     "Month DESC, <br>" +
     "State DESC, <br>" +
-    "City DESC; <br>",
+    "Payment_Type DESC; <br>",
 
 
     'Drill Down Day': "SELECT <br>"+
     "   DAY(o.order_approved_at) as 'Day', <br>" +
-    "   c.customer_state as 'State', c.customer_city as 'City', <br>" +
+    "   c.customer_state as 'State', o.payment_type as 'Payment_Type', <br>" +
     "   count(o.order_id) as 'Number of orders' <br>" +
     "FROM	<br>" +
     "   order_customers c <br>" +
@@ -226,12 +226,12 @@ const INPUT = { //with input values
     "GROUP BY <br>" +
     "   DAY(o.order_approved_at), <br>" +
     "   c.customer_state, <br>" +
-    "   c.customer_city WITH ROLLUP <br>" +
+    "   o.payment_type WITH ROLLUP <br>" +
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      null, <br>" +
     "      c.customer_state as 'State', <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -240,14 +240,14 @@ const INPUT = { //with input values
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL and  MONTH(o.order_approved_at) = MONTH_VAL <br> " +
     "   GROUP BY <br>"+
     "      c.customer_state, <br>" +
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      DAY(o.order_approved_at) as 'Day', <br>" +
     "      null, <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -256,14 +256,14 @@ const INPUT = { //with input values
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL and  MONTH(o.order_approved_at) = MONTH_VAL <br> " +
     "   GROUP BY <br>"+
     "      DAY(o.order_approved_at), <br>" +
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "UNION ( <br>" +  
     "   SELECT <br>" +
     "      null, <br>" +
     "      null, <br>" +
-    "      c.customer_city as 'City', <br>" + 
+    "      o.payment_type as 'Payment_Type', <br>" + 
     "      count(o.order_id) as 'Number of orders' <br>" + 
     "   FROM <br>" +
     "      order_customers c <br>" +
@@ -271,13 +271,13 @@ const INPUT = { //with input values
     "   INNER JOIN orders o ON o.order_id = i.order_id <br>" +
     "   WHERE YEAR(o.order_approved_at) = YEAR_VAL and  MONTH(o.order_approved_at) = MONTH_VAL <br> " +
     "   GROUP BY <br>"+
-    "      c.customer_city <br>" +
+    "      o.payment_type <br>" +
     "      WITH ROLLUP<br>" +
     ") <br>" + 
     "ORDER BY <br>" +
     "Day DESC, <br>" +
     "State DESC, <br>" +
-    "City DESC; <br>",
+    "Payment_Type DESC; <br>",
 
     "Dice": "SELECT oc.customer_state as 'State', YEAR(o.order_approved_at) as 'Year ordered', MONTH(o.order_approved_at) as 'Month ordered', count(o.order_approved_at) as 'Number of orders' <br>" +
         "FROM order_items oi <br>" +
